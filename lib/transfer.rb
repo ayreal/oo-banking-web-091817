a25c6489b3117f22d468f22ab38ca654d3b85a56
github username <github email address>
2017-09-21 19:58:41 -0400

    ginally

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,21 +1,34 @@
 class Transfer
   attr_reader :sender, :receiver
-  attr_accessor :amount, :status
+  attr_accessor :amount, :status, :executed
+
 
 def initialize(sender, receiver, amount=0, status="pending")
 @sender = sender
 @receiver = receiver
 @amount = amount
 @status = status
+@executed = false
 end
 def valid?
   sender.valid? && receiver.valid?
   # your code here
 end
 def execute_transaction
-  sender.balance = sender.balance - amount
-  receiver.balance = receiver.balance + amount
+  if valid? && sender.balance > amount && self.status == "pending"
+  sender.balance = sender.balance - self.amount
+  receiver.balance = receiver.balance + self.amount
   self.status = "complete"
-  
+else
+  self.status = "rejected"
+  "Transaction rejected. Please check your account balance."
+end
+end
+def reverse_transfer
+  if valid? && receiver.balance > amount && self.status == "complete"
+    sender.balance = sender.balance + self.amount
+    receiver.balance = receiver.balance - self.amount
+    self.status = "reversed"
+  end
 end
 end
