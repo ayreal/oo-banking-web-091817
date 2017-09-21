0f03d70ccf66e231ed1465ee3a76a9f8264b63e2
github username <github email address>
2017-09-21 17:39:42 -0400

    brain dead

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
+  attr_reader :sender, :receiver
+  attr_accessor :amount, :status
+
+def initialize(sender, receiver, amount=0, status="pending")
+@sender = sender
+@receiver = receiver
+@amount = amount
+@status = status
+end
+def valid?
+  sender.valid? && receiver.valid?
   # your code here
+end
+def execute_transaction
+  sender.balance = sender.balance - amount
+  receiver.balance = receiver.balance + amount
+  self.status = "complete"
+  
+end
 end
